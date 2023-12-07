29c1a71ebe0f0fad6db0e87b1d2afe24d82c6645
Temirlan Batyrov <cheburek.20133@gmail.com>
2023-12-07 08:01:03 +0300

    3.1

--- PZ-4/Proj_4.2sem.py
+++ PZ-4/Proj_4.2sem.py
@@ -2,14 +2,14 @@
 # деления определить, имеются ли в записи числа N нечетные цифры. Если имеются,
 # то вывести TRUE, если нет — вывести FALSE
 N = int(input('Введите целое число N: '))
-has_odd_digits = False
 
 while N > 0:
     digit = N % 10
     if digit % 2 != 0:
-        has_odd_digits = True
-
+        print(True)
+    else:
+        print(False)
     N //= 10
-    print(has_odd_digits)
+
 
 
